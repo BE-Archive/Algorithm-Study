54a83f211ab1e905c5b282f98ad95f6511e6e77a
u1qns <85053365+u1qns@users.noreply.github.com>
2024-01-22 17:41:05 +0900

    comment: BOJ_2839_설탕 배달.cpp
    줄 맞춤

--- u1qns/Week_01/BOJ_2839_설탕 배달.cpp
+++ u1qns/Week_01/BOJ_2839_설탕 배달.cpp
@@ -1,25 +1,24 @@
 #include<iostream>
 
-int main(){
-
+int main()
+{
 	int N;
-    std::cin >> N;
+    	std::cin >> N;
 	
 	int div = N/5;
 	int mod = N%5;
 	int answer = -1;
 	
 	if((N==4) || (N==7))
-        answer = -1;
+        	answer = -1;
 	else
-    {
-        if((mod == 2)||( mod ==4)) answer = div+2;
-        else if((mod==1)||(mod==3)) answer= div+1;
-        else if(mod==0) answer = div;
+    	{
+	        if((mod==2)||(mod==4)) answer = div+2;
+	        else if((mod==1)||(mod==3)) answer= div+1;
+	        else if(mod==0) answer = div;
 	}
 	
-    std::cout << answer;
+	std::cout << answer;
 
-	return 0;
-		
+	return 0;		
 }
