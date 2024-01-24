b6eb6ec4ca4268ae706d9b213cc2e386a2b10e72
beudicri <ys4512558@naver.com>
2024-01-24 22:37:00 +0900

    ✨feat: BOJ_4485_녹색 옷 입은 애가 젤다지 BFS + 풀이 추가

--- ys4512558/Week_01/BOJ_4485_녹색 옷 입은 애가 젤다지.java
+++ ys4512558/Week_01/BOJ_4485_녹색 옷 입은 애가 젤다지.java
@@ -26,9 +26,62 @@ public class Main {
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+    }
+    
+    //풀이 2 (1번 풀고 다른 사람 풀이 보고 생각나서 했음 첨부터 내 머리속에서 나온거 아님 ㅠㅠ)
+    private static long bfs_priority(int N, int[][] map) {
+        class Point implements Comparable<Point>{
+            int row;
+            int col;
+            int cost;
 
+            public Point(int row, int col, int cost) {
+                this.row = row;
+                this.col = col;
+                this.cost = cost;
+            }
+
+            @Override
+            public int compareTo(Point p) {
+                return cost - p.cost; //작은게 우선순위 높게
+            }
+        }
+
+        int[] dy = {0, -1, 0, 1};
+        int[] dx = {-1, 0, 1, 0};
+
+        boolean[][] isVisit = new boolean[N][N];
+
+        PriorityQueue<Point> queue = new PriorityQueue<>();
+        queue.add(new Point(0,0,map[0][0]));
+        isVisit[0][0] = true;
+
+        while (!queue.isEmpty()){
+            Point point = queue.poll();
+            int row = point.row;
+            int col = point.col;
+            int cost = point.cost;
+
+            for (int i = 0; i < 4; i++) {
+                int y = row + dy[i];
+                int x = col + dx[i];
+
+                if(y == N - 1 && x == N - 1){
+                    return cost + map[y][x];
+                }
+
+                if ((y < 0 || y >= N || x < 0 || x >= N) || isVisit[y][x]) {
+                    continue;
+                }
+
+                isVisit[y][x] = true;
+                queue.offer(new Point(y, x, map[y][x] + cost));
+            }
+        }
+        return 0;
     }
 
+    //풀이 1
     private static long bfs(int N, int[][] map) {
         class Point{
             int row;
