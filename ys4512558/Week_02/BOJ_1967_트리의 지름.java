cc1a75db173b3b7815896109453bb268a23ca4ea
왕승철 <ys4512558@naver.com>
2024-01-30 20:49:47 +0900

    ✨feat: BOJ_트리의_지름 2차 시도

--- ys4512558/Week_02/BOJ_1967_트리의 지름.java
+++ ys4512558/Week_02/BOJ_1967_트리의 지름.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,6 +8,71 @@ public class Main {
 }
 
 class BOJ1967 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<ArrayList<Node>> list = new ArrayList<>();
+    static int max = 0;
+
+    public static void prob1967() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // 인접 리스트 생성
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            list.get(parent).add(new Node(child, weight));
+        }
+
+        dfs(1);
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static int dfs(int currentNode) {
+        ArrayList<Node> currentList = list.get(currentNode);
+
+        int firstWeight = 0;
+        int secondWeight = 0;
+        for (Node node : currentList) {
+            int weight = dfs(node.idx) + node.weight;
+            if (firstWeight < weight) {
+                secondWeight = firstWeight;
+                firstWeight = weight;
+            } else if (secondWeight < weight) {
+                secondWeight = weight;
+            }
+        }
+        max = Math.max(max, firstWeight + secondWeight);
+        // 리프 노드는 0을 리턴하게 됨.
+        return firstWeight;
+    }
+}
+
+class Node {
+    int idx;
+    int weight;
+
+    public Node(int idx, int weight) {
+        this.idx = idx;
+        this.weight = weight;
+    }
+}
+
+/**
+ * 메모리 초과 코드
+ */
+
+class BOJ1967_first {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
@@ -44,7 +106,7 @@ class BOJ1967 {
         }
 
         for (int i = 1; i <= N; i++) {
-            res = new int[N+1];
+            res = new int[N + 1];
             for (int j = 0; j < tree.get(i).size(); j++) {
                 if (i > tree.get(i).get(j).node) {
                     continue;
@@ -67,7 +129,7 @@ class BOJ1967 {
         }
         for (int i = 0; i < tree.get(cur).size(); i++) {
             TreeNode next = list.get(i);
-            //역류 방지
+            // 역류 방지
             if (list.get(i).node < cur) {
                 continue;
             }
@@ -77,7 +139,8 @@ class BOJ1967 {
         return res[cur] + cost;
     }
 }
-class TreeNode{
+
+class TreeNode {
     int node;
     int cost;
 
