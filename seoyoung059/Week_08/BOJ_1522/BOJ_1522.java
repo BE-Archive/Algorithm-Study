4b0f781cd16a2e1c6dcb8939fa06884b857814a5
seoyoung059 <popolley@gmail.com>
2024-03-11 21:51:47 +0900

    ♻️refactor: BOJ_1522_문자열_교환

--- seoyoung059/Week_08/BOJ_1522/BOJ_1522.java
+++ seoyoung059/Week_08/BOJ_1522/BOJ_1522.java
@@ -1,2 +1,30 @@
-package Week_08.BOJ_1522;public class BOJ_1522 {
+package Week_08.BOJ_1522;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1522 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int n = str.length();
+        int cnt=0;
+        for (int i = 0; i < n; i++) {
+            if(str.charAt(i)=='b'){
+                cnt++;
+            }
+        }
+
+        int max = 0, tmp = 0;
+        for (int i = 0; i < cnt; i++) {
+            if(str.charAt(i)=='b') tmp++;
+        }
+        for (int i = 0; i < n; i++) {
+            max=Math.max(tmp, max);
+            if(str.charAt(i)=='b') tmp--;
+            if(str.charAt((i+cnt)%n)=='b') tmp++;
+        }
+        System.out.println(cnt-max);
+    }
 }
