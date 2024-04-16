ade9c0c122130efad17d6e35a98c4c47200a7c3e
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-04-17 00:42:07 +0900

    refactor : BOJ 17135 캐슬디펜스

--- u1qns/Week_13/BOJ_17135_캐슬디펜스.java
+++ u1qns/Week_13/BOJ_17135_캐슬디펜스.java
@@ -1,114 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ_17135_캐슬디펜스 {
+public class Main {
 
     static int N, M, D, answer = 0;
     static int[] pick = new int[3];
     static int[][] arr;
-
-    static int calcDistance(int[] a, int[] b)
-    {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-    }
+    static List<int[]> enemies = new ArrayList<>();
 
     static int solve()
     {
+        int castle = N;
+        int result = 0;
 
-        /**
-         * 1. 궁수들의 위치 조합을 구한다. (너비 M)에서 3개를 골라 pick에 저장한다.
-         * 2. 궁수들의 초기 위치는 (N, pick[i])에 위치하게 된다.
-         * 3. 궁수들의 공격을 하기 위해 BFS를 돌려 level D만큼 진행한다.
-         * 4. 적들이 한칸씩 내려온다 -> 궁수가 그냥 올라가면 될 일. 궁수는 N-T에 위치하게 된다.
-         *      처음 1초에는 N 에 궁수가 위치하고
-         *      2초에서 N-1 가 된다 .-> N-T; (T는 0초부터)
-         * 5. 이 짓을 몬스터가 없을 때까지 한다 -> N-T가 = 0 이 되면 안해도 되지 않겠어요 ?
-         */
+        int killPoint[][] = new int[N+1][M];
 
-        int result = 0, timer = 0;
-        boolean[][] visited = new boolean[N+1][M];
-
-        while(timer < N)
+        int x, y, offset;
+        while(castle > 0)
         {
-            int[] top = new int[3];
-            int x, y, offset, who;
-
-            // 궁수의 위치
-            Queue<int[]> q = new ArrayDeque<>();
-            q.offer(new int[]{N-timer, pick[0], 0});
-            q.offer(new int[]{N-timer, pick[1], 1});
-            q.offer(new int[]{N-timer, pick[2], 2});
-
-            Queue<int[]> best = new ArrayDeque<>(); // 각 궁수들에게 가장 가까운 몬스터
-
-            PriorityQueue<int[]> monster = new PriorityQueue<>((a, b) -> {
-                if(a[2] == b[2])
-                {
-                    if(a[0] == b[0])
-                        return Integer.compare(a[1], b[1]);
-                    return Integer.compare(b[0], a[0]);
-                }
-                return Integer.compare(a[2], b[2]);
-            });
-
-            int distance = 0;
-            while(!q.isEmpty() )
+            int tmp = 0;
+            for(int i = 0; i < 3; ++i)
             {
-                int qSize = q.size();
-                while(qSize-- > 0)
-                {
-                    top = q.poll();
-                    who = top[2];
+                int archerX = castle;
+                int archerY = pick[i];
+                int[] target = new int[] {100, 100, 100}; // x, y, offset
 
-                    monster.clear();
-                    for(int i=0; i<M; ++i)
+                for(final int[] e : enemies)
+                {
+                    x = e[0];
+                    y = e[1];
+
+                    if(x >= castle || killPoint[x][y] > castle) continue;
+
+                    offset = Math.abs(x - archerX) + Math.abs(y - archerY);
+
+                    if(offset > D) continue;
+
+                    if(target[2] > offset)
                     {
-                        x = top[0] - 1;
-                        y = i;
-                        offset = calcDistance (top, new int[] {x, y});
-                        if (offset > D || x < 0 || y < 0 || x >= N - timer || y >= M ||
-                                visited[x][y] ||  arr[x][y] == 0) continue;
-
-                        monster.add(new int[]{x, y, offset, who});
+                        target = new int[] {x, y, offset};
                     }
-
-                    if(monster.size() > 0)
-                        best.add(monster.poll());
-                    monster.clear();
+                    else if(target[2] == offset && target[1] > y)
+                    {
+                        target = new int[] {x, y, offset};
+                    }
                 }
-                if(++distance == D) break;
-                if(N-timer-distance <= 0) break;
-                q.offer(new int[]{N-timer-distance, pick[0], 0});
-                q.offer(new int[]{N-timer-distance, pick[1], 1});
-                q.offer(new int[]{N-timer-distance, pick[2], 2});
-            }
 
-            // D 레벨만큼 돈 상태니 가까운 몬스터 3마리 잡는다.
-            int cnt = 0, tmp = 0;
-            boolean order[] = new boolean[3];
-            while(!best.isEmpty())
-            {
-                top = best.poll();
-                x = top[0];
-                y = top[1];
-                who = top[3];
+                int cnt = 0;
+                if(target[0] != 100)
+                {
+                    x = target[0];
+                    y = target[1];
 
-                if(order[who]) continue;
-                order[who] = true;
-                if(visited[x][y] == false) {
+                    //System.out.printf("[KILL | %d] %d, %d\n", castle, x, y);
+
+                    // 이미 죽었던 친구라면 지나가
+                    if(killPoint[x][y] != 0) continue;
+
+                    killPoint[x][y] = castle;
+                    ++result;
+
+
                     ++cnt;
                 }
-
-                visited[x][y] = true;
-                //System.out.printf("[MONSTER] (%d, %d) %d  by %d\n", x, y, top[2], who);
-                if(++tmp == 3) break;
+                tmp += cnt;
             }
-            best.clear();
-            result += cnt;
-            //System.out.printf("result += %d => %d\n", cnt, result);
 
-            //몬스터가 내려온다. <- 궁수가 한 칸 올라간다.-> ++timer다.
-            ++timer;
+            //System.out.printf("------------------------> %d\n", tmp);
+
+            --castle;
         }
 
         return result;
@@ -117,10 +77,10 @@ public class BOJ_17135_캐슬디펜스 {
     static void comb(int depth, int idx) {
         if(depth == 3) {
 
-            // System.out.println("\n=================================");
-            //System.out.println(Arrays.toString(pick));
+            //System.out.println("\n=================================");
+            //System.out.println("pick :" + Arrays.toString(pick));
             int tmp =  solve();
-            //System.out.println("-> " + tmp);
+            //System.out.println("=> " + tmp);
             answer = Math.max(answer,tmp);
             return;
         }
@@ -140,15 +100,20 @@ public class BOJ_17135_캐슬디펜스 {
         M = Integer.parseInt(st.nextToken());
         D = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+2][M];
+        arr = new int[N+1][M];
+
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<M;j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 1) {
+                    enemies.add(new int[]{i, j});
+                }
             }
         }
-
+        
         comb(0, 0);
         System.out.print(answer);
     } // main
+
 }
