e72710c731e912c7fe37ed343250d7c004d1f88f
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-05-03 16:31:01 +0900

    ✨feat: BOJ_1081_합

---
+++ trankill1127/w15/BOJ_1081.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		long L=Integer.parseInt(st.nextToken());
+		long R=Integer.parseInt(st.nextToken());
+		System.out.print(calculate(R)-calculate(L-1));
+	}
+	
+	public static long calculate(long n) { //24660
+		if (n==0) return 0;
+		
+		long result=0;
+		long digit = 1; 
+		while (digit<n)	digit*=10; 
+		
+		long left=0;
+		while (digit>0) {
+			if (left==0) {
+				for (int i=1; i<=(n/digit); i++) {
+					if (i<(n/digit)) result+=i*digit;
+					else result+=i*(n%digit+1);
+				}
+			}
+			else {
+				for (int i=1; i<10; i++) {
+					if (i<(n/digit)) result+=(left+1)*i*digit;
+					else if (i==(n/digit)) result+=(left)*i*digit+i*(n%digit+1);
+					else result+=(left)*i*(digit);
+				}
+			}
+			
+			left*=10;
+			left+=(n/digit);
+			n=n%digit;
+			digit/=10;
+		}
+		
+		return result;
+	}
+}
