edc0d09fee8b11aa24976e4ce37919ebc8591082
lumi <26676134+l-umi@users.noreply.github.com>
2024-06-01 16:18:02 +0900

    ✨feat: BOJ_1461_도서관

---
+++ l-umi/Week_20/BOJ_1461_도서관.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+# 31120KB 32ms
+N, M = map(int, input().split())
+books = list(map(int, input().split()))
+
+left = list(filter(lambda x : x<0, books))
+left.sort()
+right = list(filter(lambda x : x>0, books))
+right.sort(reverse=True)
+
+# 마지막에 꽂을 책의 위치(왼쪽 오른쪽)
+lastBook = ""
+
+if not left: # 빈 배열일 때
+    lastBook = "right"
+elif not right or (-left[0] > right[0]): # 제일 먼 곳은 마지막에
+    lastBook = "left"
+else:
+    lastBook = "right"
+
+answer = right[0] if lastBook == "right" else -left[0]
+answer += sum(-left[i]*2 for i in range( (M if lastBook == 'left' else 0), len(left), M))
+answer += sum(right[i] * 2 for i in range( (M if lastBook == 'right' else 0), len(right), M))
+
+print(answer)
