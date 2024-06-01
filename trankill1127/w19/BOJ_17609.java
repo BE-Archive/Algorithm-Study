f66d9595752237699397137589ffe2237621c9be
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-06-01 17:59:57 +0900

    ✨feat: BOJ_17609_회문

--- trankill1127/w19/BOJ_17609.java
+++ trankill1127/w19/BOJ_17609.java
@@ -9,87 +9,36 @@ public class BOJ_17609 {
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
         String s = "";
-        for (int i=0; i<t; i++){
-            s=br.readLine();
-            int result=check(s);
+        for (int i = 0; i < t; i++) {
+            s = br.readLine();
+            int result = palindromeType(s);
             sb.append(result).append("\n");
         }
         System.out.print(sb.toString());
     }
 
-    public static int check(String s){ //summuus
-        int left = 0;
-        int right = s.length()-1;
-        int sameCnt=0;
-        int hopCnt=0;
+    public static int palindromeType(String s) {
+        int left = 0, right = s.length() - 1;
 
-        while (left<=right) {
-
-            if (s.charAt(left) == s.charAt(right)){ //같은 경우
-                sameCnt++;
-
-                left++;
-                right--;
-            }
-            else { //다른 경우
-
-                boolean leftFound=false;
-                boolean rightFound=false;
-
-                //왼쪽 확인
-                int tmpLeft = left;
-                for (; tmpLeft<=s.length()/2; tmpLeft++){
-                    if (s.charAt(tmpLeft) == s.charAt(right)) {
-                        leftFound=true;
-                        break;
-                    }
-                }
-
-                //오른쪽 확인
-                int tmpRight = right;
-                for (; s.length()/2<=tmpRight; tmpRight--){
-                    if (s.charAt(tmpRight) == s.charAt(left)) {
-                        rightFound=true;
-                        break;
-                    }
-                }
-
-                //왼쪽과 오른쪽 중 더 원래 위치에서 가까운 놈을 선택한다.
-                if (leftFound && rightFound){
-                    if ((right-tmpRight)<(tmpLeft-left)){
-                        sameCnt++;
-                        hopCnt+=(right-tmpRight);
-                    }
-                    else {
-                        sameCnt++;
-                        hopCnt+=(tmpLeft-left);
-                    }
-                }
-                else if (leftFound){
-                    sameCnt++;
-                    hopCnt+=(tmpLeft-left);
-                }
-                else if (rightFound){
-                    sameCnt++;
-                    hopCnt+=(tmpLeft-left);
+        while (left < right) {
+            if (s.charAt(left)!=s.charAt(right)){
+                if (isPalindrome(s,left+1, right) || isPalindrome(s,left,right-1)){
+                    return 1;
                 }
                 else return 2;
-
             }
+            left++; right--;
         }
 
-        if (sameCnt==s.length()/2 && hopCnt==0){
-            return 0;
-        }
-        else if (sameCnt==s.length()/2 && hopCnt==1) {
-            return 1;
-        }
-        else {
-            return 2;
-        }
+        return 0;
     }
-<<<<<<< HEAD
+
+    public static boolean isPalindrome(String s, int l, int r){
+        while (l<r) {
+            if (s.charAt(l)!=s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
 }
-=======
-}
->>>>>>> origin/trankill1127
