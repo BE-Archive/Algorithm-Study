4b3ed9a3238b958e881bf90ac610a6453b006766
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-06-07 00:13:08 +0900

    fix : BOJ 1756 피자굽기

--- u1qns/Week_20/BOJ_1756_피자굽기.cpp
+++ u1qns/Week_20/BOJ_1756_피자굽기.cpp
@@ -3,44 +3,27 @@
 #include <algorithm>
 int N, M;
 
-int getAnswer(const std::vector<int>& oven, const std::vector<int>& dough)
+int solve(const std::vector<long long>& oven)
 {
-    int depth = oven.size();
+    int depth = N;
     int cnt = 0;
-    bool isStopped = false;
-    for (const int& pizza : dough) 
+
+    long long pizza;
+    for(int i=0; i<M; ++i)
     {
-        while (--depth >= 0 && pizza > oven[depth]) {
-            ++cnt;
-            if (depth < 0) {
-                isStopped = true;
+        std::cin >> pizza;
+        while(1)
+        {
+            if(depth-- < 0) return 0;
+            if(pizza <= oven[depth])
+            {
+                ++cnt;
                 break;
             }
-        }
-        if (isStopped) {
-            break;
         }
     }
 
     return cnt == M ? depth + 1 : 0;
-}
-
-int solve(const std::vector<int>& oven)
-{
-    int answer = 0;
-    int pizza;
-    int depth = N;
-    std::vector<int> oven2(oven.rbegin(), oven.rend());
-    for(int i=0; i<M; ++i)
-    {
-        std::cin >> pizza;
-        if(pizza > oven[0]) return 0;
-        // 앞에서부터 쭉 봣을때 가장 핏한 위치가 나옴
-        int tmp = std::lower_bound(oven2.begin(), oven2.end(), pizza) - oven2.begin();
-        answer = (answer < tmp ? tmp : answer+1);
-    }
-
-  return depth - answer;
 }
 
 int main()
@@ -50,18 +33,16 @@ int main()
 
     std::cin >> N >> M;
 
-
-    int input, r = 300001;
-    std::vector<int> oven(N, 0);
+    long long input;
+    long long r = 1e9;
+    std::vector<long long> oven(N, 0);
     for(int i=0; i<N; ++i)
     {
         std::cin >> input;
         if(r > input)  r = input;
         oven[i] = r;
     }
-
     std::cout << solve(oven);
-    //std::cout << getAnswer(oven, dough);
 
     return 0;
 }
