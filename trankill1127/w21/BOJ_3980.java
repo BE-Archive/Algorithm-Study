369e59b1ac7eeab9641b6bcb8633965431c23219
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-06-10 23:07:52 +0900

    ✨feat: BOJ_3980_선발 명단

---
+++ trankill1127/w21/BOJ_3980.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3980 {
+
+    static int[][] board = new int[11][11];
+    public static int maxTotScore=0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        int c = Integer.parseInt(br.readLine());
+        while (c>0){
+            for (int i=0; i<11; i++) {
+                st = new StringTokenizer(br.readLine().trim());
+                for (int j = 0; j < 11; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+
+                }
+            }
+            maxTotScore=0;
+            recursive(0,0, new boolean[11]);
+            sb.append(maxTotScore).append("\n");
+            c--;
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void recursive(int player, int totScore, boolean[] position){
+        if (player==11){
+            maxTotScore = Math.max(maxTotScore, totScore);
+            return;
+        }
+
+        for (int pos=0; pos<11; pos++){
+            if (position[pos] || board[player][pos]==0) continue;
+            position[pos]=true;
+            recursive(player+1, totScore+board[player][pos], position);
+            position[pos]=false;
+        }
+
+    }
+}
