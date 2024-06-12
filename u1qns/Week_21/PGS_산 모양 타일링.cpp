8e34ff1a58ea73d43c54ae67f085897340abcb24
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-06-13 02:19:53 +0900

    feat : PGS 산 모양 타일링

--- u1qns/Week_21/PGS_산 모양 타일링.cpp
+++ u1qns/Week_21/PGS_산 모양 타일링.cpp
@@ -1,31 +1,24 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 int solution(int n, vector<int> tops) {
-    int answer = 0;
-    vector< vector<int> > dp(2, vector<int> (n, 0));
+    vector< vector<int> > dp(n, vector<int> (2, 0));
+    const int mod = 10007;
 
-
-    // 탑의 유무로 만들지 못하는 경우가 생김
-    dp[0][1] = 2;
-    dp[1][1] = 3;
-    for(int i=2; i<n; ++i)
+    dp[0][0] = (tops[0] ? 3 : 2);
+    dp[0][1] = 1;
+    
+    for(int i=1; i<n; ++i)
     {
-        if(tops[i-1] == 1)
-        {
-            // 직전에 탑이 있으면 모양을 더 만들 수 있지만
-            // 타일이 서로 겹치는 부분이 생기며 이 부분을 빼야함 
-        }
-        else
-        {
-          // 직전 탑이 없으면..
-        }
-            
+        dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % mod;
         
-        //cout << dp[0][i] << "\t" << dp[1][i] << "\n";
+        if(tops[i])
+            dp[i][0] = (dp[i-1][0]*3 + dp[i-1][1]*2) % mod;
+        else
+            dp[i][0] = (dp[i-1][0]*2 + dp[i-1][1]) % mod;
     }
     
+    return (dp[n-1][0]+dp[n-1][1])% mod;
 }
