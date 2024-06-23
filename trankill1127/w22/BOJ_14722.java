823d07cc8e3c8cefd48fc1087553a3bae322951a
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-06-24 01:28:39 +0900

    ♻️refactor: BOJ 14722 우유 도시

--- trankill1127/w22/BOJ_14722.java
+++ trankill1127/w22/BOJ_14722.java
@@ -3,34 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_14722 {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
 
         StringTokenizer st = null;
-        int[][] land = new int[n+1][n+1];
-        for (int i=1; i<n+1; i++){
+        int[][] land = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine().trim());
-            for (int j=1; j<n+1; j++){
-                land[i][j]=Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= n; j++) {
+                land[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][][] dp = new int[n+1][n+1][3];
-        if (land[1][1]==0) dp[1][1][0]=1;
+        int[][][] dp = new int[n + 1][n + 1][3];
+        if (land[1][1] == 0) {
+            dp[1][1][0] = 1;
+        }
 
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=n; j++){
-
-                for (int k=0; k<3; k++){
-                    dp[i][j][k]=Math.max(dp[i-1][j][k], dp[i][j-1][k]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) continue;
+                
+                for (int k = 0; k < 3; k++) {
+                    dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i][j - 1][k]);
                 }
 
                 int milk = land[i][j];
-                if (milk==0) dp[i][j][milk]=Math.max(dp[i][j][milk], dp[i][j][2]+1);
-                else if(dp[i][j][milk - 1] != 0) dp[i][j][milk]=Math.max(dp[i][j][milk], dp[i][j][milk-1]+1);
+                int befMilk = (milk + 2) % 3;
+                if (milk == 0 || dp[i][j][befMilk] != 0) {
+                    dp[i][j][milk] = Math.max(dp[i][j][milk], dp[i][j][befMilk] + 1);
+                }
             }
         }
 
