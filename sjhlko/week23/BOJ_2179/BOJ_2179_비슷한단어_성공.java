f99c31057e726afff15ea6ce658fb9c7236ff338
sjhlko <sjhlko@naver.com>
2024-06-25 12:05:03 +0900

    ✨feat : 비슷한 단어

--- sjhlko/week23/BOJ_2179/BOJ_2179_비슷한단어_성공.java
+++ sjhlko/week23/BOJ_2179/BOJ_2179_비슷한단어_성공.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BOJ_2179_비슷한단어 {
+public class BOJ_2179_비슷한단어_성공 {
     //https://www.acmicpc.net/problem/2179
     //비슷한 단어
     static int N;
+
+    static List<String> list = new ArrayList<>();
 
     static void solution() {
         String ans1= null, ans2 = null;
@@ -17,22 +19,21 @@ public class BOJ_2179_비슷한단어 {
             for (int j = i + 1; j < N; j++) {
                 int tmpCount = 0;
                 for (int k = 0; k < Math.min(list.get(i).length(),list.get(j).length()); k++) {
-                    if (list.get(i).charAt(i) == list.get(j).charAt(j)) {
+                    if (list.get(i).charAt(k) == list.get(j).charAt(k)) {
                         tmpCount++;
                     }
                     else break;
                 }
-                if (tmpCount > count) {
+                if (tmpCount > count && !list.get(i).equals(list.get(j))) {
                     ans1 = list.get(i);
                     ans2 = list.get(j);
+                    count = tmpCount;
                 }
             }
         }
         System.out.println(ans1);
         System.out.println(ans2);
     }
-
-    static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
