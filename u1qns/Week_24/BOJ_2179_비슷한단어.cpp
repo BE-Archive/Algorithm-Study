2ade51cc45f10aa3749cab9ccf5cc8fe2adb9775
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-06-25 16:02:57 +0900

    feat : BOJ 2179 비슷한 단어

--- u1qns/Week_24/BOJ_2179_비슷한단어.cpp
+++ u1qns/Week_24/BOJ_2179_비슷한단어.cpp
@@ -1,55 +1,60 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <queue>
 #include <set>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    int N;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    
     std::string T;
     std::vector<std::string> input;
     std::set<std::string> check;
-
-    //std::priority_queue<std::pair<std::string, int>, std::vector<std::pair<std::string, int>>, std::greater<>> pq;
     std::map<std::string, std::vector<int>> map;
+
+    int max_length = 0;
+    int max_prefix_order = 1e9;
+    std::string max_prefix;
+    
+    int N;
     std::cin >> N;
     input.resize(N);
-
-    int mv = 0;
-    std::string answer;
-
     for(int i=0; i<N; ++i)
     {
         std::cin >>  T;
         input[i] = T;
 
         if(check.find(T) != check.end()) continue;
-
         check.insert(T);
+
         std::string tmp="";
         for(int s=0; s<T.size(); ++s)
         {
             tmp += T[s];
             map[tmp].push_back(i);
-            if(map[tmp].size() >=2 && tmp.size() > mv)
+
+            if(map[tmp].size() > 1 && max_length < tmp.length())
             {
-                answer = tmp;
-                mv = tmp.size();
-            }
-            else if(map[tmp].size() >=2 && tmp.size() == mv)
-            {
-                if(mv!=0 && answer > tmp)
-                {
-                    answer = tmp;
-                    mv = tmp.size();
-                }
+                max_length = tmp.length();
             }
         }
     }
 
-    std::cout << input[map[answer][0]] << "\n";
-    std::cout << input[map[answer][1]] << "\n";
+    for(const auto& [key, value] : map)
+    {
+        if(value.size() >=2 && key.length() == max_length)
+        {
+            if(value[0] < max_prefix_order)
+            {
+                max_prefix_order = value[0];
+                max_prefix = key;
+            }
+        }
+    }
 
+    std::cout << input[map[max_prefix][0]] << "\n" << input[map[max_prefix][1]];
     return 0;
 }
