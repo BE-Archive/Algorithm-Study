d77c43c4e82125c160f2e20b9fee6d4de46c0459
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-07-05 01:12:58 +0900

    ✨ feat : BOJ 1041 주사위

--- u1qns/Week_24/BOJ_1041_주사위.cpp
+++ u1qns/Week_24/BOJ_1041_주사위.cpp
@@ -1,41 +1,57 @@
 #include <iostream>
+#include <algorithm>
+long long N;
+long long dice[6] = {0, };
 
-int getAnswer()
-{
-  int answer = 0;
-  
-  if(N == 1)
-  {
-    // 예외 처리 필요
+long long getAnswer(const long long min1) {
+    long long answer = 0;
+
+    if(N == 1)
+    {
+        long long mv = 0;
+        for(int i = 0; i < 6; ++i)
+        {
+            answer += dice[i];
+            mv = std::max(mv, dice[i]);
+        }
+        return answer - mv;
+    }
+
+    long long min2 = 100;
+    long long min3 = 150;
+    for(int i=0; i<6; ++i)
+    {
+        for(int j = i+1; j<6; ++j)
+        {
+            if(i+j == 5) continue;
+
+            min2 = std::min(min2, dice[i]+dice[j]);
+            for(int k=j+1; k<6; ++k)
+            {
+                if(j+k == 5 || i+k==5) continue;
+                min3 = std::min(min3, dice[i] + dice[j] + dice[k]);
+            }
+        }
+    }
+
+
+    answer += 4 * min3; // 3면
+    answer += (((N-2) * (N-2) * 5) + ((N-2) * 4)) * min1; // 2면
+    answer += (N-2) * 8 * min2;
+    answer += 4 * min2; // N-2이 2일 때를 위해 분기함. 최하단 모서리
+
     return answer;
-  }
-
-
-  // 꼭지점 8개: 상단 4개(3면), 하단 4개(2면)
-  // 면 5면 : 외부에 노출되지 않는 하단 면을 제외하면 5개, 꼭지점을 제외한 면적 (N-2)(N-2)개가 5면 존재 (1면)
-  // 선 8개: 면이 맞닿는 선에 꼭지점을 제외하면 (N-1)개가 8개 존재
-
-  /*
-    - 주사위의 평면도 위치에 따라 조합이 다름을 유의
-    1. 3면 조합 중 가장 작은 값
-    2. 2면 조합 중 가장 작은 값
-    3. 가장 작은 dice값
-
-    이 3개의 조합을 이용해서 그냥 구현하면 됨
-  */
-  
-  
-  return answer;
 }
 
 int main()
 {
-  
-  std::cin >> N;
-  for(int i=0; i<6; ++i) 
-    std::cin >> dice[i];
+    long long min1 = 50;
+    std::cin >> N;
+    for(int i = 0; i < 6; ++i)
+    {
+        std::cin >> dice[i];
+        min1 = std::min(min1, dice[i]);
+    }
 
-  std::cout << getAnswer();
-
-  return 0;
+    std::cout << getAnswer(min1);
 }
