bd7d970acb6df0af8ee6db2b60f662a6c2eab6a0
김진이 <jinniek48@gmail.com>
2024-07-22 21:10:16 +0900

    ✨feat: BOJ_1158_요세푸스 문제

---
+++ trankill1127/w25/BOJ_1158.java
@@ -0,0 +1,35 @@
+package trankill1127.w25;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1158 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		Deque<Integer> deque = new ArrayDeque();
+		for (int i = 1; i <= n; i++)
+			deque.offerLast(i);
+
+		StringBuilder sb = new StringBuilder("<");
+		int cnt = 0;
+		while (deque.size() > 0) {
+			cnt++;
+			if (cnt == k) {
+				sb.append(deque.pollFirst());
+				if (deque.size() > 0)
+					sb.append(", ");
+				else
+					sb.append(">");
+				cnt = 0;
+				continue;
+			} else
+				deque.offerLast(deque.pollFirst());
+		}
+
+		System.out.print(sb.toString());
+	}
+}
