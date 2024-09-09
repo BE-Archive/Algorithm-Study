ed6204ad72b119192d994ebb053609ee499e6ac5
김진이 <jinniek48@gmail.com>
2024-09-09 09:30:21 +0900

    ✨feat: SWEA_병사_관리 [ing]

---
+++ trankill1127/w33/SWEA_병사_관리.java
@@ -0,0 +1,20 @@
+package trankill1127.w33;
+
+public class SWEA_병사_관리 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+/*
+
+병사 id를 입력받았을 때 점수를 바로 알 수 있어야 한다.
+팀 id를 입력받았을 때 속한 병사를 바로 알 수 있어야 한다.
+
+>>>>>>>>>
+Map<Integer, List<Integer>>로 팀에 속한 병사를 관리한다.
+그리고 이걸로 각각의 병사에 접근해서 점수를 업데이트하면 되지 않을까?
+
+ */
