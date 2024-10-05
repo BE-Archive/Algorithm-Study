c4dba7a459f33f7f4ac68719e1169d64160b7fe0
sjhlko <sjhlko@naver.com>
2024-10-05 16:43:56 +0900

    ✨feat : 1,2,3 더하기 4완료

--- sjhlko/week37/BOJ_15989/LeeHi.java
+++ sjhlko/week37/BOJ_15989/LeeHi.java
@@ -10,9 +10,20 @@ public class LeeHi {
     static int N;
 
     static int solution() {
-        int[] dp = new int[N];
+        int[][] dp = new int[N+4][3];
         // 이전것 + 1,2,3 해서 N을 만들 수 있는 경우의 수
-        return dp[N];
+        dp[1][0] = 1;
+        dp[2][0] = 1;
+        dp[2][1] = 1;
+        dp[3][0] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        for (int i = 4; i <= N; i++) {
+            dp[i][0] = dp[i-1][0];
+            dp[i][1] = dp[i-2][0]+dp[i-2][1];
+            dp[i][2] = dp[i-3][0]+dp[i-3][1]+dp[i-3][2];
+        }
+        return dp[N][0] + dp[N][1] + dp[N][2];
     }
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -25,3 +36,4 @@ public class LeeHi {
     }
 
 }
+
