9c74d362a25661a50051af3721a994bce28f0b2d
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-11-22 20:25:59 +0900

    ♻refact: BOJ 1283 단축키 지정

--- u1qns/Week_44/BOJ_1283_단축키지정.java
+++ u1qns/Week_44/BOJ_1283_단축키지정.java
@@ -5,33 +5,35 @@ import java.util.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static boolean[] isCheck = new boolean[28];
+    static boolean[] isCheck = new boolean[26];
 
-    static int findShortcut(final String word, int range) {
-        for(int i=0; i<=range; ++i) {
+    static int findShortcut(final String word, final int range) {
+        for(int i=0; i<= range; ++i) {
             int idx = word.charAt(i) - 'a';
-            if(isCheck[idx]) continue;
+            if(idx<0 || idx >= 26 || isCheck[idx]) continue;
             isCheck[idx] = true;
             return i;
         }
         return -1;
     }
 
-    static int solve(String inp) {
+    static int findShortcut(final String inp) {
         String[] words = inp.toLowerCase().split(" ");
 
         // 먼저 모든 단어의 첫글자를 본다.
-        int tmp = 0;
-        for(String word : words) {
-            if(findShortcut(word, 0) != -1)
-                return tmp;
-            tmp += word.length() + 1;
+        int offset = 0;
+        for(final String word : words) {
+            int idx = findShortcut(word, 0);
+            if(idx != -1) {
+                return idx + offset;
+            }
+            offset += word.length() + 1;
         }
 
         // 왼쪽부터 단어의 문자열 탐색
         return findShortcut(inp.toLowerCase(), inp.length()-1);
     }
-    
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -39,15 +41,18 @@ public class Main {
 
         while(N -- > 0) {
             String inp = br.readLine();
-            int shortcut = solve(inp);
-            if(shortcut != -1) {
+            int shortcut = findShortcut(inp);
+            if (shortcut == -1)
+                sb.append(inp).append("\n");
+            else {
                 sb.append(inp.substring(0, shortcut))
-                        .append("[").append(inp.charAt(shortcut)).append("]")
-                        .append(inp.substring(shortcut + 1));              
-            } else
-                sb.append(inp);
-          
-            sb.append("\n");
+                        .append("[").append(inp.charAt(shortcut)).append("]");
+
+                if (shortcut + 1 < inp.length())
+                    sb.append(inp.substring(shortcut + 1));
+
+                sb.append("\n");
+            }
         }
 
         System.out.print(sb);
